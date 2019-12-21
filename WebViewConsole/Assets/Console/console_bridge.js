074464c8c022a3ec89ba6da965653bafdf57c1bd
Hubert Zhang <hubert.zyk@gmail.com>
2019-12-22 01:45:22 +0800

    Update console_bridge

--- WebViewConsole/Assets/Console/console_bridge.js
+++ WebViewConsole/Assets/Console/console_bridge.js
@@ -1,14 +1,14 @@
 (function (config) {
-        
-    if (window.__WeiboDebugConsole) return;
-    
+
+    if (window.__DebugConsole) return;
+
     function __stringifyObject(result) {
         try {
             var resultType = typeof result;
             if (resultType == 'string') {
                 return '"' + result + '"';
             } else if (resultType == 'number' ||
-                       resultType == 'boolean') {
+                resultType == 'boolean') {
                 return result;
             } else if (resultType == 'undefined') {
                 return 'undefined';
@@ -18,7 +18,7 @@
                 return result.toString();
             } else if (resultType == 'object') {
                 var cache = [];
-                var json = JSON.stringify(result, function(key, value) {
+                var json = JSON.stringify(result, function (key, value) {
                     if (typeof value === 'object' && value !== null) {
                         if (cache.indexOf(value) !== -1) {
                             // circular reference found, discard key
@@ -26,7 +26,7 @@
                         }
                         // store value in our collection
                         cache.push(value);
-                        
+
                         if (value != result) {
                             // only print first level properties
                             // otherwise you will get huge return value, when you quering things like 'window'
@@ -43,76 +43,73 @@
         }
         return result;
     }
-    
-    window.__WeiboDebugConsole = {
-    stringifyObject: function(object) {
-        return __stringifyObject(object);
-    }
-    };
-    
+
+    window.__DebugConsole = {};
+
     if (!window.console) return;
     function isNumber(n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
     }
     function __logWithParams(params) {
-        var interfaceName = config && config['bridge'];
-        if (interfaceName && window[interfaceName]) {
-            window[interfaceName].invoke('privateConsoleLog', params);
+        var interfaceName = config && config['handler'];
+        if (interfaceName) {
+            // window[interfaceName].invoke('privateConsoleLog', params);
+            window.webkit.messageHandlers[interfaceName].postMessage(params);
         }
     }
     function __updateParams(params, error) {
         var stack = error.stack;
         do {
             if (!stack.length) break;
-            
+
             var caller = stack.split("\n")[1];
-            
+
             if (!caller) break;
-            
+
             var info = caller;
-            
+
             var at_index = caller.indexOf("@");
-            
+
             if (at_index < 0 || at_index + 1 >= caller.length) {
                 info = caller;
             } else {
                 info = caller.substring(at_index + 1, caller.length);
             }
-            
+
             do {
                 function getLastNumberAndTrimInfo() {
                     var colon_index = info.lastIndexOf(":");
-                    
+
                     if (colon_index < 0 || colon_index + 1 >= info.length) {
                         return -1;
                     }
-                    
+
                     var column = info.substring(colon_index + 1);
-                    
+
                     if (!isNumber(column)) {
                         return -1;
                     }
-                    
+
                     info = info.substring(0, colon_index);
-                    
+
                     return column;
                 }
-                
+
                 // parse column no
                 var colno = getLastNumberAndTrimInfo();
                 if (colno == -1) break;
                 params.colno = colno;
-                
+
                 // parse line no
                 var lineno = getLastNumberAndTrimInfo();
                 if (lineno == -1) break;
                 params.lineno = lineno;
-                
+
                 // the rest is file
                 params.file = info;
             } while (0);
         } while (0);
-        
+
         if (!params.lineno && params.colno) {
             params.lineno = params.colno;
             delete params.colno;
@@ -135,12 +132,12 @@
             console[name] = function () {
                 var args = Array.prototype.slice.call(arguments, 0);
                 func.apply(console, args);
-                
+
                 var params = {
                     'func': name,
                     'args': __stringifyArgs(args),
                 };
-                
+
                 // retrive caller info
                 try {
                     throw Error("");
@@ -151,10 +148,10 @@
             }
         }(key));
     }
-    
+
     // catch errors
     (function () {
-        
+
         window.addEventListener('error', function (event) {
             var params = {
                 'func': 'error',
